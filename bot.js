34788f9e8b31e9114a74d434d595c63a66dfecd4
NevitOfficial <nevit.elite@gmail.com>
2018-03-31 12:08:50 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -124,4 +124,4 @@ bot.registerCommand("close", (msg, args) => {
   cooldown: "3000"
 });
 
-client.login.process.env.BOT_TOKEN);
+client.login.process.env.NDI3MTAxNzM3Mzk3MTI1MTMw.DaF7Lg.3oLsKD5kePNlGkkq1vm7Ec3Bbgc);
